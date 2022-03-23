3280266e7af34c0d849160c4c940e6e62789f809
searcy <rachel.searcy@nyu.edu>
2022-03-23 15:54:13 -0400

    Add files via upload

---
+++ tc_number_and_barcode.py
@@ -0,0 +1,29 @@
+"""tc_number_and_barcode.py Creates text file of top containers and barcodes"""
+
+#This script takes a csv export of top containers from ArchivesSpace's
+#Manage Top Containers search and returns a text file of the
+#container's type, indicator, and barcode.
+
+import csv
+
+#Replace this file with your export
+my_file = 'top-containers.csv'
+
+fh = open(my_file)
+next(fh)
+
+#Write to a text file. Comment this section out if you just want to print the results
+result_file = "my_boxes_and_barcodes.txt"
+wfh = open(result_file, 'w', newline='')
+
+#Loop through csv file. Comment out line 25 (print) if you just want the file;
+#Comment out line 26 (wfh.write) if you just want to print the results
+for line in fh:
+    items = line.split(',')
+    number_and_barcode = items[0]
+    clean_number_and_barcode = number_and_barcode[1:]
+    print(clean_number_and_barcode)
+    wfh.write(clean_number_and_barcode + '\n')
+
+wfh.close()
+fh.close()
